b01a71c9fca334f930ed5e59676aa68299f59860
DESKTOP-K18TSHP\Jaketosis <jtmarsh@asu.edu>
2020-09-26 11:29:40 -0700

    Fixing package json again 2

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-
+const views_path = path.join(__dirname, 'views');
+const static_path = path.join(__dirname, '/statichtml')
 var http = require('http'),
     fs = require('fs');
 
+app.set('view engine', 'hbs');
+app.set('views', views_path);
 
 
-app.use(express.static("statichtml"));
+app.use(express.static(static_path));
 app.get('/',(req,res)=>{
 
     //res.send(${});
