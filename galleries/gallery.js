f81df80cd52dd872e3434830080d85a3d54e9fab
DESKTOP-K18TSHP\Jaketosis <jtmarsh@asu.edu>
2020-09-26 19:51:43 -0700

    Fixing package json again 3

---
+++ galleries/gallery.js
@@ -0,0 +1,6 @@
+exports.index = function(req, res) {
+    res.render('gallery', {title: 'My Keeb Gallery'});
+   };
+   
+   exports.add_gallery = function(req, res) {
+   };
--- server.js
+++ server.js
@@ -1,25 +1,36 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+
+
+var express = require('express');
+var path = require('path');
+var gallery =require('./galleries/gallery');
+
+var app = express();
+
+app.get('/galleries',gallery.index);
+app.post('/add_gallery',gallery.add_gallery);
+
 const views_path = path.join(__dirname, 'views');
-const static_path = path.join(__dirname, '/statichtml')
+const static_path = path.join(__dirname, '/statichtml');
 var http = require('http'),
     fs = require('fs');
 
 app.set('view engine', 'hbs');
 app.set('views', views_path);
 
+const app = express();
+
+app.get('./','gallery.index')
 
 
 app.get('/',(req,res)=>{
 
-    fs.readFile('statichtml/geekhacked.html', function(err, data) {
-        if (err) {
-          throw err;
-        } else {
-          // Successful file read
-        }
-      });
+    // fs.readFile('statichtml/geekhacked.html', function(err, data) {
+    //     if (err) {
+    //       throw err;
+    //     } else {
+    //       // Successful file read
+    //     }
+    //   });
 
 });
 
